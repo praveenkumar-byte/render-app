6e100d9afa74faaf9e87c9120d72ced70e5f525e
praveenkumar-byte <praveenkumar6022@gmail.com>
2022-06-28 22:44:59 +0530

    adde3

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy/i);
   expect(linkElement).toBeInTheDocument();
 });
